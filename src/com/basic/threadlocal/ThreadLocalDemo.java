3c21c034be8ab9ba5c7963336c72cb249d3d5c65
Jun <wangzhen_tju@126.com>
2019-07-08 15:03:24 +0800

    没有使用threadlocal(和前一commit做比较)

--- src/com/basic/threadlocal/ThreadLocalDemo.java
+++ src/com/basic/threadlocal/ThreadLocalDemo.java
@@ -2,13 +2,15 @@ package com.basic.threadlocal;
 import java.util.function.Supplier;
 public class ThreadLocalDemo implements Runnable{
 	// 注意下面初始赋值
-	private ThreadLocal<Integer> localInteger = ThreadLocal.withInitial(new Supplier<Integer>() {
-		@Override
-		public Integer get() {
-			
-			return 0;
-		}
-	});
+	/*
+	 * private ThreadLocal<Integer> localInteger = ThreadLocal.withInitial(new
+	 * Supplier<Integer>() {
+	 * 
+	 * @Override public Integer get() {
+	 * 
+	 * return 0; } });
+	 */
+	private Integer localInteger = 0;
 	public static void main(String[] args) {
 		ThreadLocalDemo demo = new ThreadLocalDemo();
 		for(int i=0;i<3;i++) {
@@ -18,10 +20,10 @@ public class ThreadLocalDemo implements Runnable{
 	@Override
 	public void run() {
 		for(int i=0;i<10;i++) {
-			Integer num = this.localInteger.get();//////////////////////////////
-			num++;
-			System.out.println(Thread.currentThread().getName() + " " + num);
-			this.localInteger.set(num);//////////////////////////////
+			//Integer num = this.localInteger.get();//////////////////////////////
+			localInteger++;
+			System.out.println(Thread.currentThread().getName() + " " + localInteger);
+			//this.localInteger.set(num);//////////////////////////////
 		}
 	}
 
