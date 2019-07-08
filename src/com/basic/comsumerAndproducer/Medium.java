274e9531e4848424656725bc672174b187ccdcec
Jun <wangzhen_tju@126.com>
2019-07-08 16:49:13 +0800

    Condition对象可以唤醒指定种类的线程

--- src/com/basic/comsumerAndproducer/Medium.java
+++ src/com/basic/comsumerAndproducer/Medium.java
@@ -1,31 +1,49 @@
 package com.basic.comsumerAndproducer;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Medium {
+	
+	private Lock lock = new ReentrantLock();
+	private Condition consumerCondition = lock.newCondition();
+	private Condition producerCondition = lock.newCondition();
 	
 	private static final Integer TOTAL = 10;
 	private int num = 0;
 	
-	public synchronized void take() throws InterruptedException {
-		if(num > 0) {
-			num--;
-			System.out.println(Thread.currentThread().getName() + " has taken one, remains " + num);
-			notifyAll();
-		}
-		else {
-			System.out.println("Balance is " + num +" , set pending for " + Thread.currentThread().getName());
-			wait();
+	public void take() throws InterruptedException {
+		lock.lock();
+		try {
+			if(num > 0) {
+				num--;
+				System.out.println(Thread.currentThread().getName() + " has taken one, remains " + num);
+				producerCondition.signal();//producer只有一个
+			}
+			else {
+				System.out.println("Balance is " + num +" , set pending for " + Thread.currentThread().getName());
+				consumerCondition.await();
+			}
+		}finally {
+			lock.unlock();
 		}
 	}
 	
-	public synchronized void put() throws InterruptedException {
-		if(num < TOTAL) {
-			num++;
-			System.out.println(Thread.currentThread().getName() + " has put one, remaings " + num);
-			notifyAll();
-		}
-		else {
-			System.out.println("Buffer is full, set pending for " + Thread.currentThread().getName());
-			wait();
+	public void put() throws InterruptedException {
+		lock.lock();
+		try{
+			if(num < TOTAL) {
+				num++;
+				System.out.println(Thread.currentThread().getName() + " has put one, remaings " + num);
+				consumerCondition.signalAll();//consumer有多个
+			}
+			else {
+				System.out.println("Buffer is full, set pending for " + Thread.currentThread().getName());
+				producerCondition.await();
+			}
+		}finally {
+			lock.unlock();
 		}
 	}
 
