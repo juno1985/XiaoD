4869cfd81c5759c31294a5b68112c9c7312dafce
Jun <wangzhen_tju@126.com>
2019-07-09 10:29:41 +0800

    同前一提交(改了下结构为后面演示方便)

--- src/com/basic/container/SyncContainerDemo.java
+++ src/com/basic/container/SyncContainerDemo.java
@@ -5,26 +5,31 @@ import java.util.Vector;
 public class SyncContainerDemo {
 	
 	private Vector<String> strVector = new Vector<>();
-	
+	private final Iterator<String> vectorInter = strVector.iterator();
 	private void initVector() {
 		for(int i=0;i<1000;i++) {
-			strVector.add("element " + i);
+			this.strVector.add("element " + i);
+		}
+	}
+	
+	private void out() {
+		
+		/**
+		 * 在复合操作时如(hasNext()后使用next()),会发生异常
+		 */
+		while(this.vectorInter.hasNext()) {
+			String temp = this.vectorInter.next();
+			System.out.println(Thread.currentThread().getName() + "-->" + temp);
 		}
 	}
 
 	public static void main(String[] args) {
 		SyncContainerDemo demo = new SyncContainerDemo();
-		Iterator<String> vectorInter = demo.strVector.iterator();
 		demo.initVector();
+		
 		for(int i=0;i<4;i++) {
 			new Thread(()->{
-				/**
-				 * 在复合操作时如(hasNext()后使用next()),会发生异常
-				 */
-				while(vectorInter.hasNext()) {
-					String temp = vectorInter.next();
-					System.out.println(Thread.currentThread().getName() + "-->" + temp);
-				}
+				demo.out();
 			}).start();
 		}
 	}
